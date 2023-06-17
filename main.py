49066a23a2e94ef95821413ac565e2a30e3a5034
baananou <bananoumohamed@live.fr>
2023-06-17 18:06:13 +0100

    Done

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 import os
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, initialize_app, db
+from flask_cors import CORS, cross_origin
 
 # Initialize Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('key.json')
@@ -21,165 +23,205 @@ colorant_ref = ref.child('colorant')
 recette_ref = ref.child('recette')
 
 
-@app.route('/adduser', methods=['POST'])
-def create():
+@app.route('/user', methods=['GET'])
+@cross_origin()
+def get_user():
     """
-        create() : Add data to Firebase Realtime Database with request body.
-        Ensure you pass a custom ID as part of the JSON body in the POST request,
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    get_user() : Retrieve all data from the "user" location in Firebase Realtime Database.
     """
     try:
-        # Check if required fields are present in the JSON payload
-        if 'id' not in request.json:
-            return jsonify({"error": "Missing 'id' field"}), 400
-        if 'nom' not in request.json:
-            return jsonify({"error": "Missing 'nom' field"}), 400
-        if 'prenom' not in request.json:
-            return jsonify({"error": "Missing 'prenom' field"}), 400
-        if 'matricule' not in request.json:
-            return jsonify({"error": "Missing 'matricule' field"}), 400
+        args = request.args
+        user_data = user_ref.get()
 
-        # Extract the required fields from the JSON payload
-        id = request.json['id']
-        nom = request.json['nom']
-        prenom = request.json['prenom']
-        matricule = request.json['matricule']
+        if user_data:
+            if 'read' in args:
+                read_value = user_data.get('READ', {}).get(args['read'])
+                return jsonify({"value": read_value}), 200
 
-        # Set default values for optional fields
-        presence = request.json.get('presence', 0)
-        done = request.json.get('done', 0)
-        error = request.json.get('error', 0)
+            if 'write' in args:
+                write_value = user_data.get('WRITE', {}).get(args['write'])
+                return jsonify({"value": write_value}), 200
 
-        # Create a dictionary representing the user data
-        user_data = {
-            'nom': nom,
-            'prenom': prenom,
-            'matricule': matricule,
-            'presence': presence,
-            'done': done,
-            'error': error
-        }
-
-        # Save the user data to the Firebase Realtime Database using the custom ID
-        user_ref.child(id).set(user_data)
-
-        return jsonify({"success": True}), 200
-    except KeyError as e:
-        missing_field = str(e)
-        return jsonify({"error": f"Missing '{missing_field}' field"}), 400
+            return jsonify(user_data), 200
+        else:
+            return jsonify([]), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/getuser/<string:user_id>', methods=['GET'])
-def get_user(user_id):
+
+@app.route('/user', methods=['POST'])
+@cross_origin()
+def update_user():
     """
-        get_user(user_id) : Retrieve user data from Firebase Realtime Database based on user ID.
+    update_user() : Update data in the "user" location in Firebase Realtime Database.
     """
     try:
-        user_data = user_ref.child(user_id).get()
+        data = request.get_json()
+        key = data.get('key')
+        value = data.get('value')
+        child = data.get('child')
 
-        if user_data:
-            return jsonify(user_data), 200
+        if key and value and child:
+            if child.lower() == 'read':
+                # Update the value of the specified key in the "READ" property
+                user_ref.child('READ').update({key: value})
+            elif child.lower() == 'write':
+                # Update the value of the specified key in the "WRITE" property
+                user_ref.child('WRITE').update({key: value})
+            else:
+                return jsonify({"error": "Invalid 'child' value"}), 400
+
+            return jsonify({"message": "Value updated successfully"}), 200
         else:
-            return jsonify({"message": "User not found"}), 404
-    except Exception as e:
-        return f"An Error Occurred: {e}"
+            return jsonify({"error": "Invalid request body"}), 400
 
-@app.route('/melangeur', methods=['GET', 'POST'])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/melangeur', methods=['GET'])
+@cross_origin()
 def get_melangeur():
     """
-        get_melangeur() : Retrieve all data from the "melangeur" location in Firebase Realtime Database.
+    get_melangeur() : Retrieve all data from the "melangeur" location in Firebase Realtime Database.
     """
-    if request.method == 'POST':
-        try:
-            # Extract the request data
-            data = request.json
+    try:
+        args = request.args
+        melangeur_data = melangeur_ref.get()
 
-            # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('input', {}).items():
-                melangeur_ref.child('input').child(key).set(value)
+        if melangeur_data:
+            if 'input' in args:
+                input_value = melangeur_data.get('input', {}).get(args['input'])
+                return jsonify({"value": input_value}), 200
 
-            # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('output', {}).items():
-                melangeur_ref.child('output').child(key).set(value)
+            if 'output' in args:
+                output_value = melangeur_data.get('output', {}).get(args['output'])
+                return jsonify({"value": output_value}), 200
 
-            return jsonify({"success": True}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    elif request.method == 'GET':
-        try:
-            melangeur_data = melangeur_ref.get()
+            return jsonify(melangeur_data), 200
+        else:
+            return jsonify([]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-            if melangeur_data:
-                return jsonify(melangeur_data), 200
-            else:
-                return jsonify([]), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
 
-@app.route('/colorant', methods=['GET', 'POST'])
+@app.route('/melangeur', methods=['POST'])
+@cross_origin()
+def post_melangeur():
+    """
+    post_melangeur() : Update data in the "melangeur" location in Firebase Realtime Database.
+    """
+    try:
+        # Extract the request data
+        data = request.json
+
+        # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('input', {}).items():
+            melangeur_ref.child('input').child(key).set(value)
+
+        # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('output', {}).items():
+            melangeur_ref.child('output').child(key).set(value)
+
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/colorant', methods=['GET'])
+@cross_origin()
 def get_colorant():
     """
-        get_colorant() : Retrieve all data from the "colorant" location in Firebase Realtime Database.
+    get_colorant() : Retrieve all data from the "colorant" location in Firebase Realtime Database.
     """
-    if request.method == 'POST':
-        try:
-            # Extract the request data
-            data = request.json
+    try:
+        args = request.args
+        colorant_data = colorant_ref.get()
 
-            # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('input', {}).items():
-                colorant_ref.child('input').child(key).set(value)
+        if colorant_data:
+            if 'input' in args:
+                input_value = colorant_data.get('input', {}).get(args['input'])
+                return jsonify({"value": input_value}), 200
 
-            # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('output', {}).items():
-                colorant_ref.child('output').child(key).set(value)
+            if 'output' in args:
+                output_value = colorant_data.get('output', {}).get(args['output'])
+                return jsonify({"value": output_value}), 200
 
-            return jsonify({"success": True}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    elif request.method == 'GET':
-        try:
-            colorant_data = colorant_ref.get()
+            return jsonify(colorant_data), 200
+        else:
+            return jsonify([]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-            if colorant_data:
-                return jsonify(colorant_data), 200
-            else:
-                return jsonify([]), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
 
-@app.route('/recette', methods=['GET', 'POST'])
+@app.route('/colorant', methods=['POST'])
+@cross_origin()
+def post_colorant():
+    """
+    post_colorant() : Update data in the "colorant" location in Firebase Realtime Database.
+    """
+    try:
+        # Extract the request data
+        data = request.json
+
+        # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('input', {}).items():
+            colorant_ref.child('input').child(key).set(value)
+
+        # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('output', {}).items():
+            colorant_ref.child('output').child(key).set(value)
+
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/recette', methods=['GET'])
+@cross_origin()
 def get_recette():
     """
-        get_recette() : Retrieve all data from the "recette" location in Firebase Realtime Database.
+    get_recette() : Retrieve all data from the "recette" location in Firebase Realtime Database.
     """
-    if request.method == 'POST':
-        try:
-            # Extract the request data
-            data = request.json
+    try:
+        args = request.args
+        recette_data = recette_ref.get()
 
-            # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('input', {}).items():
-                recette_ref.child('input').child(key).set(value)
+        if recette_data:
+            if 'input' in args:
+                input_value = recette_data.get('input', {}).get(args['input'])
+                return jsonify({"value": input_value}), 200
 
-            # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
-            for key, value in data.get('output', {}).items():
-                recette_ref.child('output').child(key).set(value)
+            if 'output' in args:
+                output_value = recette_data.get('output', {}).get(args['output'])
+                return jsonify({"value": output_value}), 200
 
-            return jsonify({"success": True}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    elif request.method == 'GET':
-        try:
-            recette_data = recette_ref.get()
+            return jsonify(recette_data), 200
+        else:
+            return jsonify([]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-            if recette_data:
-                return jsonify(recette_data), 200
-            else:
-                return jsonify([]), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+
+@app.route('/recette', methods=['POST'])
+@cross_origin()
+def post_recette():
+    """
+    post_recette() : Update data in the "recette" location in Firebase Realtime Database.
+    """
+    try:
+        # Extract the request data
+        data = request.json
+
+        # Iterate over the input fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('input', {}).items():
+            recette_ref.child('input').child(key).set(value)
+
+        # Iterate over the output fields and update the corresponding fields in the Firebase Realtime Database
+        for key, value in data.get('output', {}).items():
+            recette_ref.child('output').child(key).set(value)
+
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 port = int(os.environ.get('PORT', 8080))
